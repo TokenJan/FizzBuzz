449b9c2c0041eb4f44bb64e24455cd8368964884
Jiahuan Yang <jiahuan.yang@thoughtworks.com>
2020-01-18 00:06:57 +0800

    minor refactor task 4

--- src/main/java/FizzBuzzWhizz.java
+++ src/main/java/FizzBuzzWhizz.java
@@ -3,20 +3,37 @@ public class FizzBuzzWhizz {
     private static String FIZZ = "Fizz";
     private static String BUZZ = "Buzz";
     private static String WHIZZ = "Whizz";
-    private String EMPTY_STRING = "";
+    private static String EMPTY_STRING = "";
+    private static int fizzDivider = 3;
+    private static int buzzDivider = 5;
+    private static int whizzDivider = 7;
+    private static String THREE = "3";
+    private static String FIVE = "5";
+    private static String SEVEN = "7";
+    private String result = EMPTY_STRING;
 
     public String compute(int number) {
 
-        int fizzDivider = 3;
-        int buzzDivider = 5;
-        int whizzDivider = 7;
-        String result = EMPTY_STRING;
+
         String numStr = String.valueOf(number);
 
-        if (numStr.contains("3")) {
+        // rule 4
+        if (numStr.contains(THREE)) {
             return FIZZ;
         }
 
+        // rule 2 and rule 3
+        computeRule2AndRule3(number);
+
+        // rule 1
+        if (EMPTY_STRING.equals(result)) {
+            return String.valueOf(number);
+        }
+
+        return result;
+    }
+
+    private void computeRule2AndRule3(int number) {
         if (number % fizzDivider == 0) {
             result += FIZZ;
         }
@@ -28,10 +45,5 @@ public class FizzBuzzWhizz {
         if (number % whizzDivider == 0) {
             result += WHIZZ;
         }
-        if (EMPTY_STRING.equals(result)) {
-            return String.valueOf(number);
-        }
-
-        return result;
     }
 }
